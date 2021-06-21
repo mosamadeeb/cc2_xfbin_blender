17b7f32c30adb6e5c12843ae6bc0f6d5dd285701
SutandoTsukai181 <mosamaeldeeb@gmail.com>
2021-06-21 16:31:00 +0300

    Update version and add wiki link

--- __init__.py
+++ __init__.py
@@ -5,12 +5,14 @@ blender_loader = importlib.util.find_spec('bpy')
 bl_info = {
     "name": "CyberConnect2 XFBIN File Import/Export",
     "author": "SutandoTsukai181",
-    "version": (0, 1, 0),
-    "blender": (2, 80, 0),
+    "version": (1, 0, 0),
+    "blender": (2, 90, 0),
     "location": "File > Import-Export",
     "description": "Import/Export XFBIN model files found in CyberConnect2 Naruto Storm and JoJo games.",
     "warning": "",
     "doc_url": "",
+    "wiki_url": "https://github.com/SutandoTsukai181/cc2_xfbin_blender/wiki",
+    "tracker_url": "https://github.com/SutandoTsukai181/cc2_xfbin_blender/issues",
     "category": "Import-Export",
 }
 
